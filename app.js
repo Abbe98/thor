8e8902cff582fbe2cb788b620d419698b28dd618
Abbe98 <albin.post@gmail.com>
2019-04-19 17:47:26 +0200

    custom prefix autocomplete

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 YASQE.defaults.sparql.showQueryButton = false;
 YASQE.defaults.sparql.endpoint = '';
 YASQE.defaults.value = '';
-YASQE.defaults.sparql.callbacks.success =  function(data){console.log('s', data);};
+YASQE.Autocompleters.prefixes.fetchFrom = 'prefixes.json';
+
+YASQE.defaults.sparql.callbacks.success = function(data ) {
+  console.log('s', data);
+}
 
 var yasqe = YASQE(document.getElementById('queryEditor'));
