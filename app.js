3113e3960c97275c75c65731cbf4746fe6d33d55
Abbe98 <albin.post@gmail.com>
2019-04-20 18:31:40 +0200

    property autocomplete prototype

--- app.js
+++ app.js
@@ -33,7 +33,25 @@ YASQE.Autocompleters.prefixes = function(yasqe, completerName) {
   return completer;
 };
 
-// Overriding defaults
-YASQE.defaults.autocompleters = ['prefixes'];
+const customPropertyCompleter = function(yasqe) {
+  let completer = {
+    isValidCompletionPosition: () => YASQE.Autocompleters.properties.isValidCompletionPosition(yasqe),
+    preProcessToken: (token) => YASQE.Autocompleters.properties.preProcessToken(yasqe, token),
+    postProcessToken: (token, suggestedString) => YASQE.Autocompleters.properties.postProcessToken(yasqe, token, suggestedString),
+  };
+
+  completer.bulk = true;
+  completer.async = false;
+  completer.autoShow = true;
+
+  completer.persistent = "customProperties";
+  completer.get = () => ['http://kulturarvsdata.se/ksamsok#context', 'http://kulturarvsdata.se/ksamsok#contextSuperType', 'http://kulturarvsdata.se/ksamsok#contextType'];
+
+  return completer;
+};
+
+YASQE.registerAutocompleter('customPropertyCompleter', customPropertyCompleter);
+
+YASQE.defaults.autocompleters = ['prefixes', 'customPropertyCompleter'];
 
 var yasqe = YASQE(document.getElementById('queryEditor'));
