424741c6968c6f797d8663c35fa127d9fc8c8de8
Abbe98 <albin.post@gmail.com>
2019-04-28 00:31:28 +0200

    harvest all soch properties to autocompleter

--- autocompleters/properties.js
+++ autocompleters/properties.js
@@ -1,4 +1,25 @@
-const properties = ['http://kulturarvsdata.se/ksamsok#context', 'http://kulturarvsdata.se/ksamsok#contextSuperType', 'http://kulturarvsdata.se/ksamsok#contextType'];
+const properties = [
+  'http://kulturarvsdata.se/ksamsok#context',
+  'http://kulturarvsdata.se/ksamsok#contextSuperType',
+  'http://kulturarvsdata.se/ksamsok#contextType',
+  'http://kulturarvsdata.se/ksamsok#continent',
+  'http://kulturarvsdata.se/ksamsok#country',
+  'http://kulturarvsdata.se/ksamsok#dataQuality',
+  'http://kulturarvsdata.se/ksamsok#image',
+  'http://kulturarvsdata.se/ksamsok#media',
+  'http://kulturarvsdata.se/ksamsok#itemDescription',
+  'http://kulturarvsdata.se/ksamsok#itemLicense',
+  'http://kulturarvsdata.se/ksamsok#itemMaterial',
+  'http://kulturarvsdata.se/ksamsok#itemMeasurement',
+  'http://kulturarvsdata.se/ksamsok#itemName',
+  'http://kulturarvsdata.se/ksamsok#itemNumber',
+  'http://kulturarvsdata.se/ksamsok#itemSpecification',
+  'http://kulturarvsdata.se/ksamsok#itemSuperType',
+  'http://kulturarvsdata.se/ksamsok#itemType',
+  'http://kulturarvsdata.se/ksamsok#mediaLicense',
+  'http://kulturarvsdata.se/ksamsok#subject',
+  'http://kulturarvsdata.se/ksamsok#theme',
+];
 
 const customPropertyCompleter = function(yasqe) {
   let completer = {
