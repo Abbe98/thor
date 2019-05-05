5d073c7d602c6011991e266d52298a1ae4d99a3d
Abbe98 <albin.post@gmail.com>
2019-05-05 16:21:01 +0200

    add pie chart viz

--- app.js
+++ app.js
@@ -140,6 +140,62 @@ function renderImages() {
   document.querySelector('#resultContainer').appendChild(container);
 }
 
+function renderPieChart() {
+  const data = rawResponseData.results.bindings.map(data => {
+    return {
+      label: data['label'].value,
+      count: data['count'].value,
+    };
+  });
+
+  const width = window.innerWidth;
+  const height = 500;
+
+  const color = d3.scaleOrdinal()
+    .domain(data.map(d => d.label))
+    .range(['#4285F4', '#EA4335', '#34A853', '#FBBC04', '#FA7B17', '#F53BA0', '#A142F4', '#24C1E0'])
+
+  const pie = d3.pie()
+    .sort(null)
+    .value(d => d.count)
+
+  const arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(Math.min(width, height) / 2 - 1)
+
+  arcLabel = d3.arc().innerRadius(Math.min(width, height) / 2 * 0.8).outerRadius(Math.min(width, height) / 2 * 0.8);
+  const arcs = pie(data);
+
+  const svg = d3.select('#resultContainer').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('text-anchor', 'middle')
+  .style('font', '14px sans-serif');
+
+  const g = svg.append('g')
+    .attr('transform', `translate(${width / 2},${height / 2})`);
+
+  g.selectAll('path')
+  .data(arcs)
+  .enter().append('path')
+    .attr('fill', d => color(d.data.label))
+    .attr('d', arc)
+    .attr('stroke', 'white')
+  .append('title')
+    .text(d => `${d.data.label}: ${d.data.count.toLocaleString()}`);
+
+  const text = g.selectAll('text')
+  .data(arcs)
+  .enter().append('text')
+    .attr('transform', d => `translate(${arcLabel.centroid(d)})`)
+    .attr('dy', '0.35em');
+
+  text.append('tspan')
+    .attr('x', 0)
+    .attr('y', '-0.7em')
+    .text(d => d.data.label);
+}
+
 let renderMode = 'table';
 function setRenderMode(evt) {
   renderMode = evt.options[evt.selectedIndex].value;
@@ -155,6 +211,13 @@ function render() {
       renderImages();
     } else {
       flashMessage('Could not render Image grid. No variable named "thumbnail".');
+      renderTable();
+    }
+  } else if (renderMode === 'pie') {
+    if (rawResponseData.head.vars.includes('count') && rawResponseData.head.vars.includes('label')) {
+      renderPieChart();
+    } else {
+      flashMessage('Could not render Pie chart. Missing variables "count"/"label".');
       renderTable();
     }
   } else {
