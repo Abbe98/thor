c3cc2a24b3de611c2eb122ed3c44ca9d8b9dfde6
Abbe98 <albin.post@gmail.com>
2019-06-01 20:10:39 +0200

    use unofficial cidoc ns uri

--- autocompleters/prefixes.js
+++ autocompleters/prefixes.js
@@ -6,7 +6,7 @@ const commonNamespaces = [
   'edm: <http://www.europeana.eu/schemas/edm/>',
   'dc: <http://purl.org/dc/elements/1.1/>',
   'skos: <http://www.w3.org/2004/02/skos/core#>',
-  'crm: <http://www.cidoc-crm.org/cidoc-crm/>',
+  'crm: <http://www.cidoc-crm.org/rdfs/cidoc-crm#>',
   'cc: <http://creativecommons.org/ns#>',
   'rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
   'rdfs: <http://www.w3.org/2000/01/rdf-schema#>',
